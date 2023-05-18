f62b2d7c4affec71c9c547f87f33106e593133cc
bashayer770 <beshow95@gmail.com>
2023-05-18 17:31:44 +0300

    HR PRO

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
-const employeesJSON = require('./employees.json');
-const managersJSON = require('./managers.json');
+const employeesJSON = require("./employees.json");
+const managersJSON = require("./managers.json");
 
 // Code Here
+
+class Employees {
+  constructor(name, position, yearJoined, salary) {
+    this.name = name;
+    this.position = position;
+    this.yearJoined = yearJoined;
+    this.salary = salary;
+  }
+
+  idBadge() {
+    console.log(` <${this.name}>, < ${this.position}>`);
+  }
+}
+
+class Manager extends Employees {
+  constructor(bonusPercentage, name, position, yearJoined, salary) {
+    super(name, position, yearJoined, salary);
+    this.bonusPercentage = bonusPercentage;
+  }
+
+  salaryIncrease(salaryIncrease) {
+    const ManSalary = 50 + salaryIncrease;
+    return ManSalary;
+  }
+
+  getWorkingYears(yearJoined) {
+    let workigYears = 2023 - yearJoined;
+    return workigYears;
+  }
+
+  logManagerInfo(workigYears) {
+    console.log(
+      `${this.name} since ${workigYears} , bonus ${this.bonusPercentage} `
+    );
+  }
+}
+
+const employee = new Employees("hsjs", "modeer", 2019, 5000);
+const manager = new Manager(500, "bashayer", "modeer", 2019, 5000);
+console.log(employee, manager);
+
+Employees.push(this.employee);
+Manager.push(this.Manager);
+
+Employees.forEach((em) => console.log(em));
